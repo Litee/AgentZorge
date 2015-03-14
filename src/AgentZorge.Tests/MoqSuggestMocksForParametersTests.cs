bc2cdd4747e3fdf4c4e28d74c822fa4a5d83dc4e
Andrey Lipatkin <lipatkin@gmail.com>
2015-03-14 16:13:19 +0300

    Work in progress

---
+++ src/AgentZorge.Tests/MoqSuggestMocksForParametersTests.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.ReSharper.Feature.Services.Tests.CSharp.FeatureServices.CodeCompletion;
+using JetBrains.ReSharper.TestFramework;
+using NUnit.Framework;
+
+namespace AgentZorge.Tests
+{
+    [TestFixture]
+    public class MoqSuggestMocksForParametersTests : CodeCompletionTestBase
+    {
+        [Test]
+        [TestNetFramework4]
+        [TestReferences("../Moq.dll")]
+        [TestCase("SuggestLocalMockForParameter.cs")]
+
+        public void RunAll_MoqSuggestMocksForParametersTests(string fileName)
+        {
+            DoTestFiles(fileName);
+        }
+
+        protected override bool ExecuteAction
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/AgentZorge.Tests/test/data/MoqSuggestMocksForParametersTests/SuggestLocalMockForParameter.cs
@@ -0,0 +1,44 @@
+﻿// ${COMPLETE_ITEM:It.Is}
+// ${CODE_COMPLETION_TYPE:Smart}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+
+namespace MoqExperiments
+{
+    [TestFixture]
+    public class MyTest
+    {
+
+        [Test]
+        public void Test()
+        {
+	     Mock<ITestService> _testServiceMock;
+	     new TestClass({caret}
+        }
+    }
+
+    public interface ITestService
+    {
+        void Call(string s);
+    }
+
+    public class TestClass
+    {
+        private ITestService _testService;
+
+        public TestClass(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        public void Call()
+        {
+            _testService.Call("a");
+        }
+    }
+}
